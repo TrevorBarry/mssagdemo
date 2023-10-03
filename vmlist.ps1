fede740965570031f8b8385d1fff73455126a7bd
Trevor Barry <tb@streamline-it-solutions.com>
2023-10-03 12:13:49 -0400

    Updates to fix bug

--- vmlist.ps1
+++ vmlist.ps1
@@ -9,7 +9,7 @@ $VMs = Get-VM
 
 # Loop through all VMs
 foreach ($VM in $VMs) {
-    $VM | Select-Object Name, @{N="Network"; E={$_.NetworkAdapters[0].NetworkName}}, @{N="MAC"; E={$_.NetworkAdapters[0].MacAddress}}, @{N="IP"; E={$_.Guest.IPAddress}}
+    $VM | Select-Object Name, @{N="Network"; E={$_.NetworkAdapters[0].NetworkName}}, @{N="Mac address"; E={$_.NetworkAdapters[0].MacAddress}}, @{N="IP Address"; E={$_.Guest.IPAddress}}
 }
 
 # EOF
