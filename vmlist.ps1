267eaac4b216b4bd57b66ad9f34eb331ba7131e5
Trevor Barry <tb@streamline-it-solutions.com>
2023-10-03 12:15:35 -0400

    changed line 12

--- vmlist.ps1
+++ vmlist.ps1
@@ -9,7 +9,7 @@ $VMs = Get-VM
 
 # Loop through all VMs
 foreach ($VM in $VMs) {
-    $VM | Select-Object Name, @{N="Network"; E={$_.NetworkAdapters[0].NetworkName}}, @{N="MAC"; E={$_.NetworkAdapters[0].MacAddress}}, @{N="IP"; E={$_.Guest.IPAddress}}
+    $VM | Select-Object Name, @{N="Network"; E={$_.NetworkAdapters[0].NetworkName}}, @{N="MAddr"; E={$_.NetworkAdapters[0].MacAddress}}, @{N="IPAddr"; E={$_.Guest.IPAddress}}
 }
 
 # EOF
